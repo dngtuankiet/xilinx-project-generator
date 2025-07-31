425310facfdb3842e7db034a993664a51b21e009
dngtuankiet <dngtuankiet97@gmail.com>
2025-07-31 14:31:37 +0900

    [add] basic trng

---
+++ src/basic_trng.v
@@ -0,0 +1,39 @@
+(*dont_touch = "true"*)
+module basic_trng(
+input iRst,
+input iEntropyEn,
+input iEn,
+output oRandomBit
+);
+// Ring oscillator parameters
+parameter integer FAST_RO_LENGTH = 5;
+parameter integer SLOW_RO_LENGTH = 20;
+
+// Fast ring oscillator
+wire fast_osc;
+ring_oscillator #(.FAST_RO_LENGTH) fast_ring_osc (
+    .iEn(iEntropyEn),
+    .oOsc(fast_osc)
+);
+
+// Slow ring oscillator
+wire slow_osc;
+ring_oscillator #(.SLOW_RO_LENGTH) slow_ring_osc (
+    .iEn(iEn),
+    .oOsc(slow_osc)
+);
+
+reg r_sampled_bit;
+always @(posedge slow_osc) begin
+    if(~iRst) begin
+        r_sampled_bit <= 1'b0;
+    end else if(iEn) begin
+        // Sample the fast ring oscillator output
+        r_sampled_bit <= fast_osc;
+    end
+end
+
+assign oRandomBit = r_sampled_bit;
+
+
+endmodule
--- src/ring_oscillator.v
+++ src/ring_oscillator.v
@@ -1,14 +1,19 @@
 // Simple Ring Oscillator for Arty-100T
 
 (*dont_touch = "true"*)
-module ring_oscillator (
-    output wire osc_out
+module ring_oscillator #(
+    parameter integer LENGTH = 5
+) (
+    input iEn,
+    output oOsc,
 );
-    (*dont_touch = "true"*) wire [4:0] n;
-    assign n[0] = ~n[4];
-    assign n[1] = ~n[0];
-    assign n[2] = ~n[1];
-    assign n[3] = ~n[2];
-    assign n[4] = ~n[3];
-    assign osc_out = n[4];
+    (*dont_touch = "true"*) wire [LENGTH-1:0] n;
+    integer i;
+    assign n[0] = ~(n[LENGTH-1] & iEn);
+    generate
+        for (i = 1; i < LENGTH; i = i + 1) begin : ring
+            assign n[i] = ~n[i-1];
+        end
+    endgenerate
+    assign oOsc = n[LENGTH-1];
 endmodule
